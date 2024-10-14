aeacabc23dd55f82f0a3a83ac1cd64a79a6c185f
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-15 02:16:24 +1100

    Added process and output

--- Method.js
+++ Method.js
@@ -4,3 +4,9 @@ const person1 = {
     age: 19,
     hobby: "Reading",
 }
+//Process and Output
+console.log("Name: " + person1.name);
+console.log("Age: " + person1.age);
+console.log("Hobby: " + person1.hobby);
+console.log("--------------------------------");
+
