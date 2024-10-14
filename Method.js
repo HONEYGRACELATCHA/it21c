a1ab86aa6c2e2b542eea38402e8500c1dd56ee50
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-15 02:17:29 +1100

    Added constructor

--- Method.js
+++ Method.js
@@ -9,4 +9,12 @@ console.log("Name: " + person1.name);
 console.log("Age: " + person1.age);
 console.log("Hobby: " + person1.hobby);
 console.log("--------------------------------");
+// Class definition
+class Person {
+    constructor(name, age, hobby) {
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+}
 
