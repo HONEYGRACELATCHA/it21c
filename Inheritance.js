3d1acc110a78a343998a7adb9ecbea4d09e3040f
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-16 13:25:37 +1100

    Added Getters

--- Inheritance.js
+++ Inheritance.js
@@ -10,4 +10,16 @@ class Person {
         this.#age = age;
         this.#hobbies = hobbies;
     }
+    //Getters
+    get name() {
+        return this.#name;
+    }
+
+    get age() {
+        return this.#age;
+    }
+
+    get hobbies() {
+        return this.#hobbies;
+    }
 }
